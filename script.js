3c0d1e060f4b4e85cdef68417dcd40ee213f15fe
Vishal Jha <51633569+vishaljha2121@users.noreply.github.com>
2021-09-15 23:36:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const video = document.querySelector("video");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 let countnum = 0;
-let pos = 1; //in = 0, out = 1
+let pos = 1; //left_of_box = 0, right_of_box = 1
 let color_code = "#FFFFFF";
 
 let box_pos_x = 60;
@@ -18,17 +18,38 @@ let hand_left_y_last_pos = 0;
 let hand_right_y_last_pos = 0;
 let hand_right_x_last_pos = 0;
 
+let speed_l_x = 0;
+let speed_r_x = 0;
+
 let Hcount = 0;
 let speed = 0;
 
+let dist_hand_left_x = 0;
+let dist_hand_right_x = 0;
+
 let colorb = "red";
 
-const fpsControl = FPS;
+var lastCalledTime;
+var fps;
+
+function requestAnimFrame() {
+  if (!lastCalledTime) {
+    lastCalledTime = Date.now();
+    fps = 0;
+    return;
+  }
+  delta = (Date.now() - lastCalledTime) / 1000;
+  lastCalledTime = Date.now();
+  fps = 1 / delta;
+  document.getElementById("fps").innerHTML = fps.toFixed(0);
+}
 
 function onResults(results) {
   if (!results.poseLandmarks) {
     return;
   }
+
+  requestAnimFrame();
 
   ctx.save();
   ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -37,15 +58,15 @@ function onResults(results) {
     color: color_code,
     lineWidth: point_width,
   });
-  drawLandmarks(ctx, results.poseLandmarks, {
+  drawLandmarks(ctx, results.poseLandmarks.slice(11), {
     color: "#FF0000",
     lineWidth: point_width / 2,
   });
 
-  hand_left_x = results.poseLandmarks[16].x;
-  hand_left_y = results.poseLandmarks[16].y;
-  hand_right_x = results.poseLandmarks[15].x;
-  hand_right_y = results.poseLandmarks[15].y;
+  hand_left_x = results.poseLandmarks[18].x;
+  hand_left_y = results.poseLandmarks[18].y;
+  hand_right_x = results.poseLandmarks[17].x;
+  hand_right_y = results.poseLandmarks[17].y;
 
   hitCount();
   ctx.restore();
@@ -79,7 +100,7 @@ camera.start();
 var myGamePiece;
 
 function startGame() {
-  myGamePiece = new component(60, 60, colorb, 60, 75);
+  myGamePiece = new component(57, 57, colorb, 60, 75);
   myGameArea.start();
 }
 
@@ -143,39 +164,81 @@ function updateGameArea() {
 
 function hitCount() {
   /* document.getElementById("box_x").innerHTML = (box_pos_x / 480).toFixed(2);
-  document.getElementById("box_y").innerHTML = (box_pos_y / 446).toFixed(1);
-  document.getElementById("hand_ly").innerHTML = hand_left_y.toFixed(1);
+  document.getElementById("box_y").innerHTML = (box_pos_y / 446).toFixed(1); */
+  /* document.getElementById("hand_ly").innerHTML = hand_left_y.toFixed(1);
   document.getElementById("hand_lx").innerHTML = hand_left_x.toFixed(1);
   document.getElementById("hand_ry").innerHTML = hand_right_y.toFixed(1);
   document.getElementById("hand_rx").innerHTML = hand_right_x.toFixed(1); */
-  let speed_l_x = Math.abs(hand_left_x.toFixed(1) - hand_left_x_last_pos) / 0.5;
-  let speed_r_x =
-    Math.abs(hand_right_x.toFixed(1) - hand_right_x_last_pos) / 0.5;
-
   /* document.getElementById("speed_l").innerHTML = speed_l_x.toFixed(1);
   document.getElementById("speed_r").innerHTML = speed_r_x.toFixed(1); */
 
-  if (speed_l_x < 0.1 && speed_r_x < 0.1) {
+  //SPEED CALCULATION
+  dist_hand_left_x = Math.abs(hand_left_x - hand_left_x_last_pos);
+  dist_hand_right_x = Math.abs(hand_right_x - hand_right_x_last_pos);
+
+  speed_l_x = dist_hand_left_x / 0.3;
+  speed_r_x = dist_hand_right_x / 0.3;
+
+  /* document.getElementById("right_hand_speed").innerHTML = speed_l_x.toFixed(1);
+  document.getElementById("left_hand_speed").innerHTML = speed_r_x.toFixed(1); */
+
+  //MESSAGE DISPLAY
+  /* if (speed_l_x < 0.1 && speed_r_x < 0.1) {
     document.getElementById("message").innerHTML = "Punch Harder";
   } else if (speed_l_x > 0.1 || speed_r_x > 0.1) {
     document.getElementById("message").innerHTML = "Punch Speed is good";
-  }
+  } */
 
+  //LAST HAND POSITION
   hand_left_x_last_pos = hand_left_x.toFixed(1);
   hand_left_y_last_pos = hand_left_y.toFixed(1);
   hand_right_x_last_pos = hand_right_x.toFixed(1);
   hand_right_y_last_pos = hand_right_y.toFixed(1);
 
+  //HITBOX CONDITION
+  hit_box_range_check();
+}
+
+function hit_box_range_check() {
+  bos_pos_a = 0.1;
+  bos_pos_b = 0.2;
+  bos_pos_c = (box_pos_y / 445).toFixed(1);
+  bos_pos_d = (box_pos_y / 445 + 0.1).toFixed(1);
+
+  /* document.getElementById("box_pos_a").innerHTML = bos_pos_a;
+  document.getElementById("box_pos_b").innerHTML = bos_pos_b;
+  document.getElementById("box_pos_c").innerHTML = bos_pos_c;
+  document.getElementById("box_pos_d").innerHTML = bos_pos_d; */
+
+  if (speed_l_x > 0.3 || speed_r_x > 0.3) {
+    document.getElementById("hand_speed").innerHTML = "GOOD";
+  } else if (speed_l_x < 0.1 && speed_r_x < 0.1) {
+    document.getElementById("hand_speed").innerHTML = "BAD";
+  }
+
+  if (hand_right_x < 0.1 || hand_left_x < 0.1) {
+    pos = 0;
+  } else if (hand_right_x > 0.25 && hand_left_x > 0.25) {
+    pos = 1;
+  }
+
   if (
-    (((box_pos_x / 480).toFixed(1) == hand_left_x.toFixed(1) &&
-      (box_pos_y / 445).toFixed(1) == hand_left_y.toFixed(1)) ||
-      ((box_pos_x / 480).toFixed(1) == hand_right_x.toFixed(1) &&
-        (box_pos_y / 445).toFixed(1) == hand_right_y.toFixed(1))) &&
-    (speed_l_x > 0.15 || speed_r_x > 0.15)
+    (hand_left_x < bos_pos_b &&
+      hand_left_x > bos_pos_a &&
+      hand_left_y > bos_pos_c &&
+      hand_left_y < bos_pos_d &&
+      speed_l_x > 0.2) ||
+    (hand_right_x < bos_pos_b &&
+      hand_right_x > bos_pos_a &&
+      hand_right_y > bos_pos_c &&
+      hand_right_y < bos_pos_d &&
+      speed_r_x > 0.2 &&
+      pos == 1)
   ) {
     Hcount++;
     document.getElementById("counter").innerHTML = Hcount;
 
+    //HITBOX COLOR CHANGE
     if (Hcount % 2 == 0) {
       colorb = "red";
     } else {
